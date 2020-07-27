7bd37e075c06c201005896a8fee5795827f41394
CHERNAVSKI ILYA <chernavski.ilya@gmail.com>
2020-07-27 20:38:02 +0300

    2222222

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hel2222222222222222222222o World!");
 		System.out.println("Fuck Off");
-		ssdfsdfg
+		
 	}
 }
