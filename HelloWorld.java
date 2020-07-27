d167f2ed8691af493e9772677d2f39328a84887e
CHERNAVSKI ILYA <chernavski.ilya@gmail.com>
2020-07-27 20:35:14 +0300

    Feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("He11111111llo World!");
 		System.out.println("Fuck Off");
 		ssdfsdfg
 	}
